99ad8756765b55ff4d3287ee418eee43badbd01d
Nick <32603582+nickpope85@users.noreply.github.com>
2018-04-17 11:09:04 -0700

    The Tech Academy C# Page 35 Drill
    Drill making a class with 3 methods, all doing some mathematics, then being called externally by Program.cs

---
+++ Page35Drill/Page35Drill/Methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Page35Drill
+{
+    public class Methods
+    {
+        public int Added(int numInput)
+        {
+            int addedResult = numInput + 5;
+            return addedResult;
+        }
+
+        public int Subtracted(int numInput)
+        {
+            int subtractedResult = 100 - numInput;
+            return subtractedResult;
+        }
+
+        public int Times(int numInput)
+        {
+            int timesResult = numInput * 1000;
+            return timesResult;
+        }
+    }
+}

01c6d040dc3d08a5c1c6400ae8b062c02302cfff
Nicholas Pope <nickpope85@yahoo.com>
2018-05-07 10:17:05 -0700

    Final Exercise for MVC course

---
+++ FinalExercise/FinalExercise/Context/DatabaseInitializer.cs
@@ -0,0 +1,29 @@
+﻿using FinalExercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace FinalExercise.Context
+{
+    public class DatabaseInitializer : DropCreateDatabaseIfModelChanges<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            var students = new List<Student>()
+            {
+                new Student {FirstName="Crash", LastName="Davis", DoB=DateTime.Parse("1972-01-17"), Gender=Gender.Male,},
+                new Student {FirstName="Nuke", LastName="LaLoosh", DoB=DateTime.Parse("1981-08-07"), Gender=Gender.Male,},
+                new Student {FirstName="Annie", LastName="Savoy", DoB=DateTime.Parse("1975-05-23"), Gender=Gender.Female,},
+                new Student {FirstName="Millie", LastName="Doe", DoB=DateTime.Parse("1975-03-19"), Gender=Gender.Female,},
+                new Student {FirstName="Larry", LastName="Lollygagger", DoB=DateTime.Parse("1965-10-05"), Gender=Gender.Male,}
+            };
+
+        students.ForEach(x => context.Students.Add(x));
+        context.SaveChanges();
+
+        }
+    }
+}
---
+++ FinalExercise/FinalExercise/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace FinalExercise.Models
+{
+    public class Student
+    {
+        [DisplayName("ID")] 
+        public int Id { get; set; }
+        [DisplayName("First Name")] // changes the column name
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DataType(DataType.Date)] //converts DateTime to just Date
+        [DisplayName("Date of Birth")]
+        public DateTime? DoB { get; set; } 
+        [DisplayName("Gender")]
+        public Gender Gender { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
